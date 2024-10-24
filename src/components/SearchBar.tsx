00967d3894ccf0c046eaab8d8a34de7d96bab7bc
ArthurNobrega1 <arthurnobrega906@gmail.com>
2024-10-24 08:55:19 -0300

    adding responsive data

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -15,12 +15,12 @@ export default function SearchBar() {
                         alt="Logo da Rede Social"
                         className="w-[6%] mx-3 max-md:mr-1"
                     />
-                    <p className="text-1 max-md:text-[0.4rem] w-fit">Rede Social</p>
+                    <p className="text-1 max-lg:text-[0.4rem] w-fit">Rede Social</p>
                 </div>
                 <div className="ml-3 max-md:ml-1">
                     <GrAdd className="text-neutral-500 h-4 max-md:h-2" />
                 </div>
-                <div className="flex absolute right-0 *:size-[1.5vw] *:max-md:size-[4vw]">
+                <div className="flex absolute right-0 *:size-[1.2vw] *:max-lg:size-[2vw] *:max-md:size-[4vw]">
                     <AiOutlineLine
                         className="hover:bg-[#353535] hover:text-black  p-1 rounded-md"
                     />
@@ -33,7 +33,7 @@ export default function SearchBar() {
                 </div>
 
             </div>
-            <div className="flex items-center h-4/6 bg-[#353535] text-stone-50 px-3 py-2 max-md:py-1 max-md:text-[0.4rem]">
+            <div className="flex items-center h-4/6 bg-[#353535] text-stone-50 px-3 py-2 max-md:py-1 max-lg:text-[.6rem] max-md:text-[0.4rem]">
                 <div className="flex w-full h-5/6 items-center bg-[#202020] rounded-3xl px-3 border-2 border-transparent hover:bg-[#262626] focus-within:border-blue-500">
                     <p className="text-stone-400 mr-1">https://www.redesocial.com/</p>
                     <input
--- src/components/TaskBar.tsx
+++ src/components/TaskBar.tsx
@@ -23,7 +23,7 @@ export default function TaskBar({setApp}: Props) {
   }
 
   return (
-    <div className='flex items-center justify-center gap-5 h-[12%] bg-zinc-800 rounded-b-lg'>
+    <div className='flex items-center justify-center gap-5 h-[12%] bg-zinc-800 rounded-b-lg *:max-lg:text-[130%]'>
           <HiTerminal onClick={() => handlleChangeApp('terminal')} fontSize={'190%'} className='text-[#e5e5e5] hover:text-stone-400 ease-in-out duration-300 cursor-pointer active:scale-75 active:text-[#e5e5e5] max-md:max-h-4'/>
           <BsBrowserChrome onClick={() => handlleChangeApp('socialMedia')} fontSize={'170%'} className='text-[#e5e5e5] hover:text-stone-400 ease-in-out duration-300 cursor-pointer active:scale-75 active:text-[#e5e5e5] max-md:max-h-3'/>
         </div>
