c2c14e00e86f4b4a45486a20d73e224256f6bd15
ArthurNobrega1 <arthurnobrega906@gmail.com>
2024-11-07 11:55:05 -0300

    add outher types of toast

--- src/utils/toast.ts
+++ src/utils/toast.ts
@@ -1,12 +1,49 @@
-import toast from "react-hot-toast"
+import toast from "react-hot-toast";
 
+// Toast de informação
 export const showInfoToast = (message: string, duration = 4000) => {
     toast(message, {
         icon: 'ℹ️',
         style: {
-            background: '#1E40AF',
+            background: '#1E40AF', // Azul
             color: '#FFFFFF'
         },
         duration: duration
-    })
+    });
+}
+
+// Toast de alerta
+export const showWarnToast = (message: string, duration = 4000) => {
+    toast(message, {
+        icon: '⚠️',
+        style: {
+            background: '#F59E0B', // Amarelo
+            color: '#000000'
+        },
+        duration: duration
+    });
+}
+
+// Toast de erro
+export const showErrorToast = (message: string, duration = 4000) => {
+    toast(message, {
+        icon: '❌',
+        style: {
+            background: '#DC2626', // Vermelho
+            color: '#FFFFFF'
+        },
+        duration: duration
+    });
+}
+
+// Toast de sucesso
+export const showSuccessToast = (message: string, duration = 4000) => {
+    toast(message, {
+        icon: '✅',
+        style: {
+            background: '#16A34A', // Verde
+            color: '#FFFFFF'
+        },
+        duration: duration
+    });
 }
