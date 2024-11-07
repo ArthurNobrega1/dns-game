84eb729e75398bcc82f133f2a218165cc0551a41
ArthurNobrega1 <arthurnobrega906@gmail.com>
2024-11-07 11:50:29 -0300

    adding handle end game

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import { useState } from "react"
 import Home from "./screens/Home"
 import Game from "./screens/Game"
+import { Toaster } from "react-hot-toast"
 
 function App() {
   const [screen, setScreen] = useState('home')
 
   return (
     <>
+      <Toaster />
       {
         screen === 'home'
           ? <Home setScreen={setScreen} />
-          : <Game />
+          : <Game setScreen={setScreen} />
       }
     </>
   )
--- src/screens/Game.tsx
+++ src/screens/Game.tsx
@@ -1,14 +1,29 @@
-import { Toaster } from "react-hot-toast"
 import Computer from "../components/Computer"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { showInfoToast } from "../utils/toast"
 
-export default function Game() {
+interface Props {
+    setScreen: React.Dispatch<React.SetStateAction<string>>
+}
+
+export default function Game({ setScreen }: Props) {
     const [displayFirstHash, setDisplayFirstHash] = useState(0)
     const [displaySecondHash, setDisplaySecondHash] = useState(0)
 
+    useEffect(() => {
+        if (displayFirstHash === 1 && displaySecondHash === 1) {
+            showInfoToast('Parabéns! Você encontrou o IP de sua rede social e agora pode acessá-la', 3000)
+            setTimeout(() => {
+                showInfoToast('Jogue novamente...', 3000)
+                setScreen('home')
+                setDisplayFirstHash(0)
+                setDisplaySecondHash(0)
+            }, 3000)
+        }
+    }, [displayFirstHash, displaySecondHash])
+
     return (
         <div className="relative size-full flex items-center justify-center gap-3 bg-gradient-to-r from-fuchsia-500 to-cyan-500">
-            <Toaster />
             <div className="absolute flex top-2 right-2">
                 <div className="w-[5rem]">
                     <p style={{ opacity: displayFirstHash }} className="torn-paper pl-2 py-1 ease-in-out duration-700">142.250</p>
