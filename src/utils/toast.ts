f80046ca44c5a70e0af3c4f51f48445c6b4f446a
ArthurNobrega1 <arthurnobrega906@gmail.com>
2024-11-07 08:56:10 -0300

    fix toast utils

--- src/utils/toast.ts
+++ src/utils/toast.ts
@@ -1,11 +1,12 @@
 import toast from "react-hot-toast"
 
-export const showInfoToast = (message: string) => {
+export const showInfoToast = (message: string, duration = 4000) => {
     toast(message, {
         icon: 'ℹ️',
         style: {
             background: '#1E40AF',
             color: '#FFFFFF'
         },
+        duration: duration
     })
 }
