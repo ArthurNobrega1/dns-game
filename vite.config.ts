874cacfd48f05b7cd871657807571fb19f17829b
ArthurNobrega1 <arthurnobrega906@gmail.com>
2024-11-11 16:31:41 -0300

    add support for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/dns-game/', // substitua <nome-do-repositorio> pelo nome do seu repositório
 })
