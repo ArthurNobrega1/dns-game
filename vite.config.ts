f8bc89cbe9f2f4d969a10025f9a034abee1d7bf0
ArthurNobrega1 <arthurnobrega906@gmail.com>
2024-11-11 18:03:05 -0300

    refactor vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dns-game/', // substitua <nome-do-repositorio> pelo nome do seu repositório
+  base: '/dns-game/'
 })
